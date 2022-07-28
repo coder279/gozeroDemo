1c9a6cecc46513566f3d4963df627075253432a4
‘coder279’ <coder279@126.com>
2022-07-28 14:16:29 +0800

    :sparkles: feat:Added order interface module

--- service/cmd.bat
+++ service/cmd.bat
@@ -1,5 +1,6 @@
 start go run user/rpc/user.go -f user/rpc/etc/user.yaml
 start go run user/api/user.go -f user/api/etc/user.yaml
-start go run product/rpc/product.go -f user/rpc/etc/product.yaml
-start go run product/api/product.go -f user/api/etc/product.yaml
-
+start go run product/rpc/product.go -f product/rpc/etc/product.yaml
+start go run product/api/product.go -f product/api/etc/product.yaml
+start go run order/rpc/order.go -f order/rpc/etc/order.yaml
+start go run order/api/order.go -f order/api/etc/order.yaml
--- service/order/rpc/orderclient/order.go
+++ service/order/rpc/orderclient/order.go
@@ -8,7 +8,7 @@ import (
 
 	"gozeroDemo/service/order/rpc/order"
 
-	"github.com/tal-tech/go-zero/zrpc"
+	"github.com/zeromicro/go-zero/zrpc"
 	"google.golang.org/grpc"
 )
 
